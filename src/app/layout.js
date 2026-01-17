820064dcca9bdca4929e769923aaed24d8330c65
lakpao5 <lakpasherpa9865@gmail.com>
2026-01-17 15:16:44 +0545

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "Lakpa Electronics",
+  description: "electronic shop",
+};
+
+const RootLayout=({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+};
+export default RootLayout;
